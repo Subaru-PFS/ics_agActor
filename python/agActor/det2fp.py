c0fef48375338a64b2860f4ec3fb184db947563e
Hiroshige Yoshida <hiro@naoj.org>
2020-10-12 06:17:50 -1000

    Correct position and numbering of the AG cameras and fix detector to focal plane coordinate transformation code

--- python/agActor/det2fp.py
+++ python/agActor/det2fp.py
@@ -1,17 +1,17 @@
 import numpy
 
 
-# sines & cosines of 0, 60, 120, 180, 240, & 300 deg
-_SIN60 = numpy.array([0, 0.8660254037844386, 0.8660254037844386, 0, -0.8660254037844386, -0.8660254037844386])
-_COS60 = numpy.array([1, 0.5, -0.5, -1, -0.5, 0.5])
+# sines & cosines of 30, 90, 150, 210, 270, & 330 deg
+_SIN60 = numpy.array([0.5, 1, 0.5, -0.5, -1, -0.5])
+_COS60 = numpy.array([0.8660254037844386, 0, -0.8660254037844386, -0.8660254037844386, 0, 0.8660254037844386])
 
-# camera 1 at 6 o'clock position (0, 241.292) mm
-# camera 2 at 4 o'clock position (-121, 209) mm
-# camera 3 at 2 o'clock position (-121, -209) mm
-# camera 4 at 12 o'clock position (0, -241.292) mm
-# camera 5 at 10 o'clock position (121, -209) mm
-# camera 6 at 8 o'clock position (121, 209) mm
-# (ref. kawanomoto-san's autoguider.pdf)
+# camera 1 at 9 o'clock position (241.292, 0) mm
+# camera 2 at 7 o'clock position (120.646, 208.965) mm
+# camera 3 at 5 o'clock position (-120.646, 208.965) mm
+# camera 4 at 3 o'clock position (-241.292, 0) mm
+# camera 5 at 1 o'clock position (-120.646, -208.965) mm
+# camera 6 at 11 o'clock position (120.646, -208.965) mm
+# (ref. morimoto-san's memo_AGcamera_20171023.pdf)
 
 
 def det2fp(icam, x_det, y_det):
@@ -42,11 +42,11 @@ def det2fp(icam, x_det, y_det):
 
     p = 0.013  # mm
     r = 241.292  # mm
-    a = numpy.mod(3 - icam, 6).astype(numpy.intc)
+    a = numpy.mod(1 - icam, 6).astype(numpy.intc)
     sin_a = _SIN60[a]
     cos_a = _COS60[a]
     x = (x_det - 511.5) * p
-    y = (y_det - 511.5) * p - r
+    y = - (y_det - 511.5) * p + r
     x_fp = cos_a * x + sin_a * y
     y_fp = - sin_a * x + cos_a * y
     return x_fp, y_fp
