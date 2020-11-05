db7fbfeee58d48aab6a305c0cca2f2ffbd76aaf6
Hiroshige Yoshida <hiro@naoj.org>
2020-11-05 09:14:04 -1000

    Incorporate Kawanomoto-san's new celestial <-> focal plane code with updated optics model

--- python/agActor/kawanomoto/FieldAcquisition.py
+++ python/agActor/kawanomoto/FieldAcquisition.py
@@ -4,7 +4,7 @@ import sys
 from . import Subaru_POPT2_HSC
 
 class PFS():
-    def FA(self, stararray, detectarray, tel_ra, tel_de, dt, adc, inr, envtmp=273.15, wl=0.77):
+    def FA(self, stararray, detectarray, tel_ra, tel_de, dt, adc, inr, tmp=273.15, wl=0.77):
         # subaru = Subaru_POPT2_HSC.Subaru()
         popt2  = Subaru_POPT2_HSC.POPT2()
         # hsc    = Subaru_POPT2_HSC.HSC()
@@ -12,7 +12,7 @@ class PFS():
         str_xdp,str_ydp,dxra,dyra,dxde,dyde,dxinr,dyinr = \
             popt2.makeBasis(tel_ra, tel_de, \
                             stararray[:,0], stararray[:,1], \
-                            dt, adc, inr, envtmp, wl)
+                            dt, adc, inr, tmp, wl)
         filtered_detectarray = popt2.sourceFilter(detectarray)
         ra_offset,de_offset,inr_offset = \
             popt2.RADECInRShift(filtered_detectarray[:,2],\
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     ### ccdid objectid xcent[mm] ycent[mm] flx[counts] semimajor[pix] semiminor[pix]
     ###
     ###### telescope status
-    ### DATE-OBS UT-STR RA2000 DEC2000 ADC-STR INR-STR
+    ### DATE-OBS UT-STR RA2000 DEC2000 ADC-STR INR-STR ENV-TEMP WAVELENGTH
     ###
 
     starfile = sys.argv[1]
@@ -48,9 +48,9 @@ if __name__ == "__main__":
 
     pfs = PFS()
 
-    tel_ra, tel_de, dt, adc, inr = popt2.telStatusfromText(telstat)
+    tel_ra, tel_de, dt, adc, inr, envtmp, wl = popt2.telStatusfromText(telstat)
 
-    ra_offset,de_offset,inr_offset = pfs.FA(stararray, agarray, tel_ra, tel_de, dt, adc, inr )
+    ra_offset,de_offset,inr_offset = pfs.FA(stararray, agarray, tel_ra, tel_de, dt, adc, inr , envtmp, wl)
 
     # print("#", ra_offset,de_offset,inr_offset+Subaru_POPT2_HSC.hsc_inr_zero_offset )
     # print("R.A. offset = %f [deg]" %( ra_offset))
