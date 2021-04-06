a40c01f1f9ac116675b6392cbaf946a67ac6fb26
Hiroshige Yoshida <hiro@naoj.org>
2021-04-05 15:04:42 -1000

    Use PostgreSQL and pfs_utils for simulated operational database based on the production system

--- python/agActor/opdb.py
+++ python/agActor/opdb.py
@@ -1,81 +1,123 @@
-import datetime
-import os
-import sqlite3
+import pfs.utils.opdb
 
 
-class opDB:
+class opDB(pfs.utils.opdb.opDB):
 
-    _DATABASE = os.path.expandvars('$ICS_MHS_DATA_ROOT/opdb/agc.db')
-
-    _DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f%z'
+    # override hostname
+    pfs.utils.opdb.opDB.host = 'localhost'
 
     @staticmethod
-    def _connect(database=_DATABASE):
+    def _fetchall(stmt, params=None):
 
-        return sqlite3.connect(database)
+        with opDB.connect() as conn:
+            with conn.cursor() as curs:
+                curs.execute(stmt, params)
+                return curs.fetchall()
 
     @staticmethod
-    def insert_tile(tile_id, ra_center, dec_center, pa):
+    def _fetchone(stmt, params=None):
 
-        program_id = tile_id
-        tile = tile_id
-        is_finished = False
-        with opDB._connect() as c:
-            c.execute('INSERT INTO tile (tile_id,program_id,tile,ra_center,dec_center,pa,is_finished) VALUES (?,?,?,?,?,?,?)', (tile_id, program_id, tile, ra_center, dec_center, pa, int(is_finished)))
-            c.commit()
+        with opDB.connect() as conn:
+            with conn.cursor() as curs:
+                curs.execute(stmt, params)
+                return curs.fetchone()
 
     @staticmethod
     def query_tile(tile_id):
 
-        with opDB._connect() as c:
-            return c.execute('SELECT ra_center,dec_center,pa FROM tile WHERE tile_id=?', (tile_id, )).fetchone()
+        return opDB._fetchone(
+            'SELECT ra_center,dec_center,pa FROM tile WHERE tile_id=%s', (tile_id,)
+        )
 
     @staticmethod
-    def insert_guide_star(tile_id, data):
+    def query_guide_object(tile_id):
 
-        with opDB._connect() as c:
-            c.executemany('INSERT INTO guide_star (tile_id,source_id,ra,dec,mag) VALUES (?,?,?,?,?)', ((tile_id, *row) for row in data))
-            c.commit()
+        return opDB._fetchall(
+            'SELECT source_id,ra,decl,mag FROM guide_object WHERE tile_id=%s', (tile_id,)
+        )
 
-    @staticmethod
-    def query_guide_star(tile_id):
-
-        with opDB._connect() as c:
-            return c.execute('SELECT source_id,ra,dec,mag FROM guide_star WHERE tile_id=?', (tile_id, )).fetchall()
-
-    @staticmethod
-    def insert_agc_exposure(frame_id, pfs_visit_id, exptime, taken_at, azimuth, altitude, inr, adc, inside_temperature, inside_humidity, inside_pressure, outside_temperature, outside_humidity, outside_pressure, m2_pos3):
-
-        with opDB._connect() as c:
-            c.execute('INSERT INTO agc_exposure (frame_id,pfs_visit_id,exptime,taken_at,azimuth,altitude,inr,adc,inside_temperature,inside_humidity,inside_pressure,outside_temperature,outside_humidity,outside_pressure,m2_pos3) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', (frame_id, pfs_visit_id, exptime, taken_at.astimezone(tz=datetime.timezone.utc).strftime(opDB._DATETIME_FORMAT), azimuth, altitude, inr, adc, inside_temperature, inside_humidity, inside_pressure, outside_temperature, outside_humidity, outside_pressure, m2_pos3))
-            c.commit()
+    query_guide_star = query_guide_object
 
     @staticmethod
     def query_agc_exposure(frame_id):
 
-        with opDB._connect() as c:
-            data = c.execute('SELECT pfs_visit_id,exptime,taken_at,azimuth,altitude,inr,adc,inside_temperature,inside_humidity,inside_pressure,outside_temperature,outside_humidity,outside_pressure,m2_pos3 FROM agc_exposure WHERE frame_id=?', (frame_id, )).fetchone()
-            m2_pos3 = data[-1]
-            if m2_pos3 is None:
-                m2_pos3 = 5.5  # nominally 5-6 mm for HSC
-            return (*data[:2], datetime.datetime.strptime(data[2], opDB._DATETIME_FORMAT), *data[3:-1], m2_pos3)
-
-    @staticmethod
-    def insert_agc_data(frame_id, data):
-
-        with opDB._connect() as c:
-            c.executemany('INSERT INTO agc_data (frame_id,camera_id,spot_id,image_moment_00,centroid_x,centroid_y,central_image_moment_11,central_image_moment_20,central_image_moment_02,peak_pixel_x,peak_pixel_y,peak_intensity,background,stepped) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)', ((frame_id, *row) for row in data))
-            c.commit()
+        return opDB._fetchone(
+            'SELECT pfs_visit_id,exptime,taken_at,azimuth,altitude,inr,adc,inside_temperature,inside_humidity,inside_pressure,outside_temperature,outside_humidity,outside_pressure,m2_pos3 FROM agc_exposure WHERE frame_id=%s', (frame_id,)
+        )
 
     @staticmethod
     def query_agc_data(frame_id):
 
-        with opDB._connect() as c:
-            data = c.execute('SELECT camera_id,spot_id,image_moment_00,centroid_x,centroid_y,central_image_moment_11,central_image_moment_20,central_image_moment_02,peak_pixel_x,peak_pixel_y,peak_intensity,background,stepped FROM agc_data WHERE frame_id=?', (frame_id, )).fetchall()
-            return [(*x[:-1], int(x[3] >= 511.5)) if x[-1] is None else x for x in data]
+        return opDB._fetchall(
+            'SELECT camera_id,spot_id,image_moment_00,centroid_x,centroid_y,central_image_moment_11,central_image_moment_20,central_image_moment_02,peak_pixel_x,peak_pixel_y,peak_intensity,background,stepped FROM agc_data WHERE frame_id=%s', (frame_id,)
+        )
 
-    # spt_operational_database-like method names
-    get_tile = query_tile
-    get_guide_star = query_guide_star
-    get_agc_exposure = query_agc_exposure
-    get_agc_data = query_agc_data
+    @staticmethod
+    def insert_tile(tile_id, ra_center, dec_center, pa):
+
+        params = locals().copy()
+        params.update(
+            program_id=tile_id,
+            tile=tile_id,
+            is_finished=False
+        )
+        opDB.insert('tile', **params)
+
+    @staticmethod
+    def insert_guide_object(tile_id, data):
+
+        for x in data:
+            params = dict(
+                tile_id=tile_id,
+                source_id=int(x[0]),
+                ra=float(x[1]),
+                decl=float(x[2]),
+                mag=float(x[3])
+            )
+            opDB.insert('guide_object', **params)
+
+    insert_guide_star = insert_guide_object
+
+    @staticmethod
+    def insert_agc_exposure(
+            frame_id,
+            pfs_visit_id,
+            exptime,
+            taken_at,
+            azimuth,
+            altitude,
+            inr,
+            adc,
+            inside_temperature,
+            inside_humidity,
+            inside_pressure,
+            outside_temperature,
+            outside_humidity,
+            outside_pressure,
+            m2_pos3
+    ):
+
+        params = locals()
+        opDB.insert('agc_exposure', **params)
+
+    @staticmethod
+    def insert_agc_data(frame_id, data):
+
+        for x in data:
+            params = dict(
+                frame_id=frame_id,
+                camera_id=int(x[0]),
+                spot_id=int(x[1]),
+                image_moment_00=float(x[2]),
+                centroid_x=float(x[3]),
+                centroid_y=float(x[4]),
+                central_image_moment_11=float(x[5]),
+                central_image_moment_20=float(x[6]),
+                central_image_moment_02=float(x[7]),
+                peak_pixel_x=int(x[8]),
+                peak_pixel_y=int(x[9]),
+                peak_intensity=int(x[10]),
+                background=float(x[11]),
+                stepped=bool(x[12])
+            )
+            opDB.insert('agc_data', **params)
