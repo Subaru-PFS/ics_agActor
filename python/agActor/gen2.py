5d84994ae91170090322d76340a5e601284d82a1
Hiroshige Yoshida <hiro@naoj.org>
2021-11-10 12:55:56 -1000

    Fix handling of 'None' received es MHS status values

--- python/agActor/gen2.py
+++ python/agActor/gen2.py
@@ -20,13 +20,13 @@ class Gen2:
             key.timestamp,
             key.isCurrent,
             key.isGenuine,
-            [x.__class__.baseType(x) for x in key.valueList]
+            [x.__class__.baseType(x) if x is not None else None for x in key.valueList]
         ))
 
     def _getValues(self, key):
 
         valueList = self.actor.models['gen2'].keyVarDict[key].valueList
-        return {x.name: x.__class__.baseType(x) for x in valueList} if len(valueList) > 1 else valueList[0].__class__.baseType(valueList[0])
+        return {x.name: x.__class__.baseType(x) if x is not None else None for x in valueList} if len(valueList) > 1 else valueList[0].__class__.baseType(valueList[0]) if valueList[0] is not None else None
 
     @property
     def visit(self):
