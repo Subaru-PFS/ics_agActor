c56aa83aca53218a5b80817d288682e5db719093
Hiroshige Yoshida <hiro@naoj.org>
2022-04-27 11:29:20 -1000

    Merge OTF and non-OTF field acquisition commands

--- python/agActor/Commands/AgCmd.py
+++ python/agActor/Commands/AgCmd.py
@@ -17,8 +17,7 @@ class AgCmd:
             ('ping', '', self.ping),
             ('status', '', self.status),
             ('show', '', self.show),
-            ('acquire_field', '[<design_id>] [<design_path>] [<visit_id>] [<exposure_time>] [<guide>]', self.acquire_field),
-            ('acquire_field', '@otf [<visit_id>] [<exposure_time>] [<guide>] [<center>] [<magnitude>]', self.acquire_field_otf),
+            ('acquire_field', '[<design_id>] [<design_path>] [<visit_id>] [<exposure_time>] [<guide>] [<center>] [<magnitude>]', self.acquire_field),
             ('focus', '[<visit_id>] [<exposure_time>]', self.focus),
             ('autoguide', '@start [<design_id>] [<design_path>] [<visit_id>] [<from_sky>] [<exposure_time>] [<cadence>] [<focus>]', self.start_autoguide),
             ('autoguide', '@initialize [<design_id>] [<design_path>] [<visit_id>] [<from_sky>] [<exposure_time>] [<cadence>] [<focus>]', self.initialize_autoguide),
@@ -88,10 +87,7 @@ class AgCmd:
         design_path = None
         if 'design_path' in cmd.cmd.keywords:
             design_path = str(cmd.cmd.keywords['design_path'].values[0])
-        if all(x is None for x in (design_id, design_path)):
-            cmd.fail('text="AgCmd.acquire_field: <design_id> and/or <design_path> required"')
-            return
-        design = (design_id, design_path)
+        design = (design_id, design_path) if any(x is not None for x in (design_id, design_path)) else None
         visit_id = self.visit_id
         if 'visit_id' in cmd.cmd.keywords:
             visit_id = int(cmd.cmd.keywords['visit_id'].values[0])
@@ -104,6 +100,12 @@ class AgCmd:
         guide = False
         if 'guide' in cmd.cmd.keywords:
             guide = bool(cmd.cmd.keywords['guide'].values[0])
+        center = None
+        if 'center' in cmd.cmd.keywords:
+            center = tuple([float(x) for x in cmd.cmd.keywords['center'].values])
+        magnitude = None
+        if 'magnitude' in cmd.cmd.keywords:
+            magnitude = float(cmd.cmd.keywords['magnitude'].values[0])
 
         try:
             cmd.inform('exposureTime={}'.format(exposure_time))
@@ -126,6 +128,8 @@ class AgCmd:
                     tel_status = self.actor.gen2.tel_status
                     self.actor.logger.info('AgCmd.acquire_field: tel_status={}'.format(tel_status))
                     kwargs['tel_status'] = tel_status
+                    if center is None:
+                        center = tel_status[5:8]
                 if self.with_opdb_tel_status:
                     status_update = self.actor.gen2.statusUpdate
                     status_id = (status_update['visit'], status_update['sequence'])
@@ -143,6 +147,10 @@ class AgCmd:
             self.actor.logger.info('AgCmd.acquire_field: dataTime={}'.format(data_time))
             if self.with_agcc_timestamp:
                 kwargs['taken_at'] = data_time  # unix timestamp, not timezone-aware datetime
+            if center is not None:
+                kwargs['center'] = center
+            if magnitude is not None:
+                kwargs['magnitude'] = magnitude
             # retrieve field center coordinates from opdb
             # retrieve exposure information from opdb
             # retrieve guide star coordinates from opdb
@@ -198,7 +206,7 @@ class AgCmd:
                 )
             if self.with_opdb_agc_match:
                 data_utils.write_agc_match(
-                    design_id=design_id if design_id is not None else pfs_design.to_design_id(design_path),
+                    design_id=design_id if design_id is not None else pfs_design.to_design_id(design_path) if design_path is not None else 0,
                     frame_id=frame_id,
                     guide_objects=values[0],
                     detected_objects=values[1],
@@ -206,133 +214,6 @@ class AgCmd:
                 )
         except Exception as e:
             cmd.fail('text="AgCmd.acquire_field: {}'.format(e))
-            return
-        cmd.finish()
-
-    def acquire_field_otf(self, cmd):
-
-        controller = self.actor.controllers['ag']
-        #self.actor.logger.info('controller={}'.format(controller))
-        mode = controller.get_mode()
-        if mode != controller.Mode.OFF:
-            cmd.fail('text="AgCmd.acquire_field_otf: mode={}'.format(mode))
-            return
-
-        visit_id = self.visit_id
-        if 'visit_id' in cmd.cmd.keywords:
-            visit_id = int(cmd.cmd.keywords['visit_id'].values[0])
-            self.visit_id = visit_id
-        exposure_time = 2000  # ms
-        if 'exposure_time' in cmd.cmd.keywords:
-            exposure_time = int(cmd.cmd.keywords['exposure_time'].values[0])
-            if exposure_time < 100:
-                exposure_time = 100
-        guide = False
-        if 'guide' in cmd.cmd.keywords:
-            guide = bool(cmd.cmd.keywords['guide'].values[0])
-        ra, dec, pa = None, None, -90.0
-        if 'center' in cmd.cmd.keywords:
-            ra = float(cmd.cmd.keywords['center'].values[0])
-            dec = float(cmd.cmd.keywords['center'].values[1])
-            if len(cmd.cmd.keywords['center'].values) > 2:
-                pa = float(cmd.cmd.keywords['center'].values[2])
-        magnitude = 20.0
-        if 'magnitude' in cmd.cmd.keywords:
-            magnitude = float(cmd.cmd.keywords['magnitude'].values[0])
-
-        try:
-            cmd.inform('exposureTime={}'.format(exposure_time))
-            # start an exposure
-            result = self.actor.queueCommand(
-                actor='agcc',
-                cmdStr='expose object pfsVisitId={} exptime={} centroid=1'.format(visit_id, exposure_time / 1000),
-                timeLim=(exposure_time // 1000 + 5)
-            )
-            kwargs = {}
-            # update gen2 status values
-            time.sleep(exposure_time / 1000 / 2)
-            self.actor.queueCommand(
-                actor='gen2',
-                cmdStr='updateTelStatus',
-                timeLim=5
-            ).get()
-            tel_status = self.actor.gen2.tel_status
-            self.actor.logger.info('AgCmd.acquire_field_otf: tel_status={}'.format(tel_status))
-            kwargs['tel_status'] = tel_status
-            if any(x is None for x in (ra, dec)):
-                ra, dec, pa, *_ = tel_status[5:]
-            kwargs['center'] = ra, dec, pa
-            telescope_state = None
-            if self.with_mlp1_status:
-                telescope_state = self.actor.mlp1.telescopeState
-                self.actor.logger.info('AgCmd.acquire_field_otf: telescopeState={}'.format(telescope_state))
-            # wait for an exposure to complete
-            result.get()
-            frame_id = self.actor.agcc.frameId
-            self.actor.logger.info('AgCmd.acquire_field_otf: frameId={}'.format(frame_id))
-            data_time = self.actor.agcc.dataTime
-            self.actor.logger.info('AgCmd.acquire_field_otf: dataTime={}'.format(data_time))
-            if self.with_agcc_timestamp:
-                kwargs['taken_at'] = data_time  # unix timestamp, not timezone-aware datetime
-            kwargs['magnitude'] = magnitude
-            dalt = daz = None
-            if guide:
-                cmd.inform('detectionState=1')
-                # convert equatorial coordinates to horizontal coordinates
-                _, _, _, dra, ddec, dinr, dalt, daz, *values = field_acquisition.acquire_field(frame_id=frame_id, altazimuth=True, logger=self.actor.logger, **kwargs)
-                cmd.inform('text="dra={},ddec={},dinr={},dalt={},daz={}"'.format(dra, ddec, dinr, dalt, daz))
-                filenames = ('/dev/shm/guide_objects.npy', '/dev/shm/detected_objects.npy', '/dev/shm/identified_objects.npy')
-                for filename, value in zip(filenames, values):
-                    numpy.save(filename, value)
-                cmd.inform('data={},{},{},"{}","{}","{}"'.format(ra, dec, pa, *filenames))
-                cmd.inform('detectionState=0')
-                dx, dy, size, peak, flux = values[3], values[4], values[5], values[6], values[7]
-                # send corrections to mlp1 and gen2 (or iic)
-                result = self.actor.queueCommand(
-                    actor='mlp1',
-                    # daz, dalt: arcsec, positive feedback; dx, dy: mas, HSC -> PFS; size: mas; peak, flux: adu
-                    cmdStr='guide azel={},{} ready=1 time={} delay=0 xy={},{} size={} intensity={} flux={}'.format(- daz, - dalt, data_time, dx / 98e-6, - dy / 98e-6, size * 13 / 98e-3, peak, flux),
-                    timeLim=5
-                )
-                result.get()
-                #cmd.inform('guideReady=1')
-            else:
-                cmd.inform('detectionState=1')
-                _, _, _, dra, ddec, dinr, *values = field_acquisition.acquire_field(frame_id=frame_id, logger=self.actor.logger, **kwargs)
-                cmd.inform('text="dra={},ddec={},dinr={}"'.format(dra, ddec, dinr))
-                filenames = ('/dev/shm/guide_objects.npy', '/dev/shm/detected_objects.npy', '/dev/shm/identified_objects.npy')
-                for filename, value in zip(filenames, values):
-                    numpy.save(filename, value)
-                cmd.inform('data={},{},{},"{}","{}","{}"'.format(ra, dec, pa, *filenames))
-                cmd.inform('detectionState=0')
-                # send corrections to gen2 (or iic)
-            # store results in opdb
-            if self.with_opdb_agc_guide_offset:
-                # always compute focus offset and tilt
-                dz, dzs = _focus._focus(detected_objects=values[1], logger=self.actor.logger)
-                data_utils.write_agc_guide_offset(
-                    frame_id=frame_id,
-                    ra=ra,
-                    dec=dec,
-                    pa=pa,
-                    delta_ra=dra,
-                    delta_dec=ddec,
-                    delta_insrot=dinr,
-                    delta_az=daz,
-                    delta_el=dalt,
-                    delta_z=dz,
-                    delta_zs=dzs
-                )
-            if self.with_opdb_agc_match:
-                data_utils.write_agc_match(
-                    design_id=0,
-                    frame_id=frame_id,
-                    guide_objects=values[0],
-                    detected_objects=values[1],
-                    identified_objects=values[2]
-                )
-        except Exception as e:
-            cmd.fail('text="AgCmd.acquire_field_otf: {}'.format(e))
             return
         cmd.finish()
 
