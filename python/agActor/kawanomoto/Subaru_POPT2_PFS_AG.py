d55baf65706f83796665cac166e092eeec2e6e65
Craig Loomis <cloomis@astro.princeton.edu>
2024-10-29 01:46:17 -1000

    fix import of Subaru_POPT2_PFS

--- python/agActor/kawanomoto/Subaru_POPT2_PFS_AG.py
+++ python/agActor/kawanomoto/Subaru_POPT2_PFS_AG.py
@@ -9,7 +9,7 @@ import astropy.coordinates as ac
 from astropy.utils import iers
 iers.conf.auto_download = False
 
-import Subaru_POPT2_PFS as pfs
+from . import Subaru_POPT2_PFS as pfs
 
 ### ccd pixel size
 ccdpxsz  = 0.013
