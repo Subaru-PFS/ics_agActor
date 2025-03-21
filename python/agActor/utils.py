a6b7507c692a6c39fd8dfaa2c6e37695004b5d5b
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-03-20 20:16:42 -1000

    * Copy over the AutoGuiderStarMask enum

--- python/agActor/utils.py
+++ python/agActor/utils.py
@@ -1,18 +1,17 @@
 # mapping of keys and value types between field_acquisition.py and FieldAcquisitionAndFocusing.py
 from datetime import datetime, timezone
+from enum import IntFlag
 from numbers import Number
 
 import numpy as np
 import pandas as pd
+from agActor import _gen2_gaia as gaia
+from agActor.opdb import opDB as opdb
+from agActor.pfs_design import pfsDesign as pfs_design
 from astropy import units
 from astropy.coordinates import AltAz, Angle, SkyCoord
 from astropy.table import Table
 from astropy.time import Time
-
-from agActor import _gen2_gaia as gaia
-from agActor.opdb import opDB as opdb
-from agActor.pfs_design import pfsDesign as pfs_design
-from pfs.datamodel.guideStars import AutoGuiderStarMask
 
 _KEYMAP = {
     'fit_dinr': ('inrflag', int),
@@ -29,6 +28,41 @@ FILENAMES = {
     'detected_objects': '/dev/shm/detected_objects.npy',
     'identified_objects': '/dev/shm/identified_objects.npy'
 }
+
+
+# TODO Use a shared version from pfs.datamodel.
+class AutoGuiderStarMask(IntFlag):
+    """
+    Represents a bitmask for guide star properties.
+
+    Attributes:
+        GAIA: Gaia DR3 catalog.
+        HSC: HSC PDR3 catalog.
+        PMRA: Proper motion RA is measured.
+        PMRA_SIG: Proper motion RA measurement is significant (SNR>5).
+        PMDEC: Proper motion Dec is measured.
+        PMDEC_SIG: Proper motion Dec measurement is significant (SNR>5).
+        PARA: Parallax is measured.
+        PARA_SIG: Parallax measurement is significant (SNR>5).
+        ASTROMETRIC: Astrometric excess noise is small (astrometric_excess_noise<1.0).
+        ASTROMETRIC_SIG: Astrometric excess noise is significant (astrometric_excess_noise_sig>2.0).
+        NON_BINARY: Not a binary system (RUWE<1.4).
+        PHOTO_SIG: Photometric measurement is significant (SNR>5).
+        GALAXY: Is a galaxy candidate.
+    """
+    GAIA = 0x00001
+    HSC = 0x00002
+    PMRA = 0x00004
+    PMRA_SIG = 0x00008
+    PMDEC = 0x00010
+    PMDEC_SIG = 0x00020
+    PARA = 0x00040
+    PARA_SIG = 0x00080
+    ASTROMETRIC = 0x00100
+    ASTROMETRIC_SIG = 0x00200
+    NON_BINARY = 0x00400
+    PHOTO_SIG = 0x00800
+    GALAXY = 0x01000
 
 
 def parse_kwargs(kwargs: dict) -> None:
