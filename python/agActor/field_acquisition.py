30c7667b9a6f61c03ccca2ae2858daa2539d103f
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-06-26 11:40:14 -1000

    * Remove `AutoGuiderStarMask` from local code and use from `pfs_utils.`

--- python/agActor/field_acquisition.py
+++ python/agActor/field_acquisition.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timezone
-from enum import IntFlag
 from numbers import Number
 
 import numpy
@@ -14,6 +13,8 @@ import to_altaz
 from kawanomoto import FieldAcquisitionAndFocusing
 from opdb import opDB as opdb
 from pfs_design import pfsDesign as pfs_design
+from pfs.utils.datamodel.ag import AutoGuiderStarMask
+
 
 # mapping of keys and value types between field_acquisition.py and FieldAcquisitionAndFocusing.py
 _KEYMAP = {
@@ -24,44 +25,6 @@ _KEYMAP = {
     "min_size": ("minsize", float),
     "max_residual": ("maxresid", float),
 }
-
-
-class AutoGuiderStarMask(IntFlag):
-    """
-    Represents a bitmask for guide star properties.
-    Attributes:
-        GAIA: Gaia DR3 catalog.
-        HSC: HSC PDR3 catalog.
-        PMRA: Proper motion RA is measured.
-        PMRA_SIG: Proper motion RA measurement is significant (SNR>5).
-        PMDEC: Proper motion Dec is measured.
-        PMDEC_SIG: Proper motion Dec measurement is significant (SNR>5).
-        PARA: Parallax is measured.
-        PARA_SIG: Parallax measurement is significant (SNR>5).
-        ASTROMETRIC: Astrometric excess noise is small (astrometric_excess_noise<1.0).
-        ASTROMETRIC_SIG: Astrometric excess noise is significant (astrometric_excess_noise_sig>2.0).
-        NON_BINARY: Not a binary system (RUWE<1.4).
-        PHOTO_SIG: Photometric measurement is significant (SNR>5).
-        GALAXY: Is a galaxy candidate.
-    """
-
-    GAIA = 0x00001
-    HSC = 0x00002
-    PMRA = 0x00004
-    PMRA_SIG = 0x00008
-    PMDEC = 0x00010
-    PMDEC_SIG = 0x00020
-    PARA = 0x00040
-    PARA_SIG = 0x00080
-    ASTROMETRIC = 0x00100
-    ASTROMETRIC_SIG = 0x00200
-    NON_BINARY = 0x00400
-    PHOTO_SIG = 0x00800
-    GALAXY = 0x01000
-    MAX_ELLIPTICITY = 0x02000
-    MAX_SIZE = 0x04000
-    MIN_SIZE = 0x08000
-    MAX_RESID = 0x10000
 
 
 def _filter_kwargs(kwargs):
