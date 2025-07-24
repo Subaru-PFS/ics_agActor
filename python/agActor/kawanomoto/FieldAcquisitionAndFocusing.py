d79f137a833ece5378d3a7ea46ec00f7a8d69f1f
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-07-24 13:36:44 -1000

    * Remove stale and unused code from files in `kawanomoto` directory. These are duplicated from what is now in `pfs_utils` and are out of date and therefore misleading. Note that these are also the lines that were mostly preventing `black` from being run.

--- python/agActor/kawanomoto/FieldAcquisitionAndFocusing.py
+++ python/agActor/kawanomoto/FieldAcquisitionAndFocusing.py
@@ -1,66 +1,11 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-from astropy import units as u
-from astropy.coordinates import AltAz, EarthLocation, SkyCoord
 from pfs.utils.coordinates import Subaru_POPT2_PFS
 
 from . import Subaru_POPT2_PFS_AG
 
-### Subaru location
-sbr_lat =   +19.8255
-sbr_lon =  +204.523972222
-sbr_hei = +4163.0
-Lsbr = EarthLocation(lat=sbr_lat,lon=sbr_lon,height=sbr_hei)
 
-### misc.
-sbr_press  = 620.0
-
-### Telescope mount model parameters (2023/04/29)
-PFS_a0 =  0.314306
-PFS_a1 =  0.038765
-PFS_a2 = -0.006086
-PFS_a3 = -0.001102
-PFS_a4 =  0.004851
-PFS_a5 =  0.006647
-PFS_a6 = -0.006588
-
-class PFS():
-    def FA(self, carray, darray, tel_ra, tel_de, dt, adc, inr, m2pos3, wl, inrflag=1, scaleflag=0, maxellip=0.6, maxsize=20.0, minsize=0.92, maxresid=0.5):
-        tel_coord = SkyCoord(ra=tel_ra, dec=tel_de, unit=(u.deg, u.deg), frame='icrs')
-        frame_subaru = AltAz(obstime  = dt, location = Lsbr,\
-                                pressure = sbr_press*u.hPa, obswl = wl*u.micron)
-        tel_altaz = tel_coord.transform_to(frame_subaru)
-        az = tel_altaz.az.degree+180.0
-        el = tel_altaz.alt.degree
-
-        PFS_beta = (np.arctan2((PFS_a3-(PFS_a4+PFS_a5)*np.cos(az/180*np.pi)),(PFS_a2+(PFS_a4+PFS_a5)*np.sin(az/180*np.pi))))*180/np.pi+180.0
-        PFS_z = np.sqrt((PFS_a3-(PFS_a4+PFS_a5)*np.cos(az/180*np.pi))**2+(PFS_a2+(PFS_a4+PFS_a5)*np.sin(az/180*np.pi))**2)
-        PFS_eps = np.arctan2((np.sin(PFS_z/180*np.pi)*(np.sin(az/180*np.pi-PFS_beta/180*np.pi))) , (np.cos(PFS_z/180*np.pi)*np.cos(el/180*np.pi)-np.sin(PFS_z/180*np.pi)*np.sin(el/180*np.pi)*np.cos(az/180*np.pi-PFS_beta/180*np.pi)))/np.pi*180
-
-        pfs  = Subaru_POPT2_PFS_AG.PFS()
-        v_0, v_1 = \
-            pfs.makeBasis(tel_ra, tel_de, \
-                          carray[:,0], carray[:,1], \
-                          dt, adc, inr + PFS_eps, m2pos3, wl)
-
-        v_0 = (np.insert(v_0,2, carray[:,2], axis=1))
-        v_1 = (np.insert(v_1,2, carray[:,2], axis=1))
-
-        filtered_darray, v = pfs.sourceFilter(darray, maxellip, maxsize, minsize)
-
-        ra_offset,de_offset,inr_offset, scale_offset, mr = \
-            pfs.RADECInRShiftA(filtered_darray[:,2],\
-                                   filtered_darray[:,3],\
-                                   filtered_darray[:,4],\
-                                   filtered_darray[:,7],\
-                                   v_0, v_1,\
-                                   inrflag, scaleflag, maxresid)
-
-        rs = mr[:,6]**2+mr[:,7]**2
-        rs[mr[:,8]==0.0]=np.nan
-        md = np.nanmedian(np.sqrt(rs))
-
-        return ra_offset,de_offset,inr_offset, scale_offset, mr, md, v
+class PFS:
 
     def FAinstpa(self, carray, darray, tel_ra, tel_de, dt, adc, instpa, m2pos3, wl, inrflag=1, scaleflag=0, maxellip=0.6, maxsize=20.0, minsize=0.92, maxresid=0.5):
         subaru = Subaru_POPT2_PFS.Subaru()
