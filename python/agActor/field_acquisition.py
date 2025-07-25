e7db2fbbd7d24f702968fb33068be1881d69cd97
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-07-24 23:02:55 -1000

    Clean up repo - use pfs_utils for AutoGuiderStarMask

--- python/agActor/field_acquisition.py
+++ python/agActor/field_acquisition.py
@@ -1,11 +1,11 @@
 from datetime import datetime, timezone
-from enum import IntFlag
 from numbers import Number
 
 import numpy as np
 from astropy.table import Table
 from numpy.lib import recfunctions as rfn
 from pfs.utils.coordinates import coordinates
+from pfs.utils.datamodel.ag import AutoGuiderStarMask
 
 from agActor.catalog import gen2_gaia as gaia
 from agActor.catalog.pfs_design import pfsDesign as pfs_design
@@ -23,44 +23,6 @@ _KEYMAP = {
     "min_size": ("minsize", float),
     "max_residual": ("maxresid", float),
 }
-
-
-class AutoGuiderStarMask(IntFlag):
-    """
-    Represents a bitmask for guide star properties.
-    Attributes:
-        GAIA: Gaia DR3 catalog.
-        HSC: HSC PDR3 catalog.
-        PMRA: Proper motion RA is measured.
-        PMRA_SIG: Proper motion RA measurement is significant (SNR>5).
-        PMDEC: Proper motion Dec is measured.
-        PMDEC_SIG: Proper motion Dec measurement is significant (SNR>5).
-        PARA: Parallax is measured.
-        PARA_SIG: Parallax measurement is significant (SNR>5).
-        ASTROMETRIC: Astrometric excess noise is small (astrometric_excess_noise<1.0).
-        ASTROMETRIC_SIG: Astrometric excess noise is significant (astrometric_excess_noise_sig>2.0).
-        NON_BINARY: Not a binary system (RUWE<1.4).
-        PHOTO_SIG: Photometric measurement is significant (SNR>5).
-        GALAXY: Is a galaxy candidate.
-    """
-
-    GAIA = 0x00001
-    HSC = 0x00002
-    PMRA = 0x00004
-    PMRA_SIG = 0x00008
-    PMDEC = 0x00010
-    PMDEC_SIG = 0x00020
-    PARA = 0x00040
-    PARA_SIG = 0x00080
-    ASTROMETRIC = 0x00100
-    ASTROMETRIC_SIG = 0x00200
-    NON_BINARY = 0x00400
-    PHOTO_SIG = 0x00800
-    GALAXY = 0x01000
-    MAX_ELLIPTICITY = 0x02000
-    MAX_SIZE = 0x04000
-    MIN_SIZE = 0x08000
-    MAX_RESID = 0x10000
 
 
 def filter_kwargs(kwargs):
@@ -138,6 +100,7 @@ def acquire_field(*, frame_id, obswl=0.62, altazimuth=False, logger=None, **kwar
     log_message(logger, f"Got {len(detected_objects)} detected objects")
 
     # Check we have detected objects and all flags are <= 1 (right-side flag).
+    # TODO fix logical error here.
     if len(detected_objects) == 0 and all([d[-1] <= 1 for d in detected_objects]):
         raise RuntimeError("No valid spots detected, can't compute offsets")
 
