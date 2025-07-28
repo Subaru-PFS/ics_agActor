9c53ab29956c73b4bc5673211ba8e2a2e8bb4d0c
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-07-28 07:55:42 -1000

    * Cleaning up obfuscated code where functions are called in the middle of a `return` statement. * Move two instances of the `semi_axes` inner function into a shared math utility file. * Changing `semi_axes` function to use eigenvalues. * Adding types

---
+++ python/agActor/utils/math.py
@@ -0,0 +1,52 @@
+"""
+Mathematical utility functions for the agActor package.
+"""
+
+from typing import Tuple
+
+import numpy as np
+
+
+def semi_axes(xy: float, x2: float, y2: float) -> Tuple[float, float]:
+    """
+    Calculate the semi-major and semi-minor axes of an ellipse using eigenvalues.
+    
+    This function computes the semi-axes of an ellipse from the second moments
+    of a 2D distribution by calculating the eigenvalues of the covariance matrix.
+    
+    Parameters
+    ----------
+    xy : float
+        Mixed central moment (covariance) of the distribution.
+    x2 : float
+        Second central moment in x (variance in x).
+    y2 : float
+        Second central moment in y (variance in y).
+    
+    Returns
+    -------
+    Tuple[float, float]
+        A tuple containing the semi-major and semi-minor axes (a, b).
+        
+    Notes
+    -----
+    The covariance matrix is formed as:
+    [x2  xy]
+    [xy  y2]
+    
+    The eigenvalues of this matrix are used to calculate the semi-axes.
+    """
+    covariance_matrix = np.array([[x2, xy],
+                                  [xy, y2]])
+
+    # eigh returns (eigenvalues, eigenvectors)
+    # Eigenvalues are guaranteed to be real and sorted in ascending order
+    eigenvalues = np.linalg.eigh(covariance_matrix)[0]
+
+    # The semi-axes are the square roots of the eigenvalues.
+    # Since they are sorted, eigenvalues[1] will be the larger one (semi-major)
+    # and eigenvalues[0] will be the smaller one (semi-minor).
+    a = np.sqrt(eigenvalues[1]) # Semi-major axis
+    b = np.sqrt(eigenvalues[0]) # Semi-minor axis
+
+    return a, b
