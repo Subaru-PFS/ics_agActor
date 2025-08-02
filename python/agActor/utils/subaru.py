663fe8b3cc77b0fbebd6ef155f2f60a1e9f8c76c
Wilfred Tyler Gee <wtylergee@gmail.com>
2025-08-01 20:25:08 -1000

    Use SUBARU location from pfs_utils
    * Removes the `subaru` file from ics_agActor and replaces with `pfs_utils` common version.

--- python/agActor/utils/subaru.py
+++
@@ -1,8 +0,0 @@
-from astropy import units as u
-from astropy.coordinates import Angle, EarthLocation
-
-# subaru coordinates (NAD83 ~ WGS 1984 at 0.1" level, height of elevation axis)
-height = elevation = 4163 * u.m
-latitude = Angle("+19:49:31.8", unit=u.deg)
-longitude = Angle("-155:28:33.7", unit=u.deg)
-location = EarthLocation(lat=latitude, lon=longitude, height=height)
